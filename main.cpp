38bea28f2cb98d1ea7ba76cd68a86a17c6885e02
MahadiAnkon <ankon1807075@stud.kuet.ac.bd>
2023-11-18 13:50:17 +0600

    new object created and drawer open and close

--- main.cpp
+++ main.cpp
@@ -73,6 +73,21 @@ unsigned int floor_tex;
 unsigned int painting_tex;
 unsigned int painting_tex2;
 unsigned int painting_tex3;
+unsigned int bed_frame;
+unsigned int bedsheet;
+unsigned int bedframe;
+unsigned int pillow;
+unsigned int table_tex;
+unsigned int brick_wood;
+bool box=false;
+bool box2 = false;
+bool box3 = false;
+bool box4 = false;
+float shamne1=0.0;
+float shamne2 = 0.0;
+float shamne3 = 0.0;
+float shamne4 = 0.0;
+float aquarium;
 //Camera
 Camera camera(glm::vec3(0.0f, 0.0f, 1.5f));
 float lastX = SCR_WIDTH / 2.0f;
@@ -89,7 +104,7 @@ BasicCamera basic_camera(eyeX, eyeY, eyeZ, lookAtX, lookAtY, lookAtZ, V);
 glm::vec3 LightPositions[] = {
     glm::vec3(1.50f,  1.50f,  -4.0f),
     glm::vec3(1.50f,  1.50f,  1.0f),
-    glm::vec3(-1.5f,  1.5f,  -4.0f),
+    glm::vec3(-5.5f,  1.5f,  -2.0f),
     glm::vec3(-1.5f,  1.5f,  1.0f),
 };
 DirLight dirlight(
@@ -142,8 +157,8 @@ PointLight pointlight2(
 
 
 // light settings
-bool pointLightOn1 = true;
-bool pointLightOn2 = false;
+bool pointLightOn1 = false;
+bool pointLightOn2 = true;
 bool dirLightOn = true;
 bool spotLightOn = true;
 bool ambientToggle = true;
@@ -547,32 +562,27 @@ void ladder(Cube& cube, Shader& lightingShader, Shader& lightingShader2, glm::ma
 
 void dressingtable(Cube& cube, Shader& lightingShader, Shader& lightingShader2, glm::mat4 model)
 {
+    cube.setTextureProperty(table_tex, table_tex, 32.0f);
     glm::mat4 a = transform(-2.0, -.8, -4, 0, 0, 0, 3.5, 2.75, 0.1);
-    cube.setMaterialisticProperty(glm::vec3(0.631, 0.255, 0.008));
-    cube.drawCubeWithMaterialisticProperty(lightingShader2, model * a);
+    cube.drawCubeWithTexture(lightingShader, model * a);
 
     a = transform(-2.0, -.8, -4, 0, 0, 0, 0.1, 1.25, 1.2);
-    cube.setMaterialisticProperty(glm::vec3(0.561, 0.224, 0));
-    cube.drawCubeWithMaterialisticProperty(lightingShader2, model * a);
+    cube.drawCubeWithTexture(lightingShader, model * a);
 
     a = transform(-0.3, -.8, -4, 0, 0, 0, 0.1, 1.25, 1.2);
-    cube.setMaterialisticProperty(glm::vec3(0.561, 0.224, 0));
-    cube.drawCubeWithMaterialisticProperty(lightingShader2, model * a);
+    cube.drawCubeWithTexture(lightingShader, model * a);
 
     a = transform(-1.15, -.8, -4, 0, 0, 0, 0.1, 1.25, 1.2);
-    cube.setMaterialisticProperty(glm::vec3(0.561, 0.224, 0));
-    cube.drawCubeWithMaterialisticProperty(lightingShader2, model * a);
+    cube.drawCubeWithTexture(lightingShader, model * a);
 
     a = transform(-1.15, -.5, -4, 0, 0, 0, 1.7, 0.1, 1.2);
-    cube.setMaterialisticProperty(glm::vec3(0.561, 0.224, 0));
-    cube.drawCubeWithMaterialisticProperty(lightingShader2, model * a);
+    cube.drawCubeWithTexture(lightingShader, model * a);
 
     a = transform(-2.0, -.8, -3.4, 0, 0, 0, 1.7, 1.25, 0.1);
     //  drawCube(cubeVAO, lightingShader, model * a, 1, 0, 0);
 
     a = transform(-2, -.2, -4, 0, 0, 0, 3.5, 0.1, 1.2);
-    cube.setMaterialisticProperty(glm::vec3(0.631, 0.263, 0.016));
-    cube.drawCubeWithMaterialisticProperty(lightingShader2, model * a);
+    cube.drawCubeWithTexture(lightingShader, model * a);
 
 }
 
@@ -589,33 +599,32 @@ void bed(Cube& cube, Shader& lightingShader, Shader& lightingShader2, glm::mat4 
     float headHeight = 0.6;
 
     //base
+    cube.setTextureProperty(bed_frame, bed_frame, 32.0f);
     glm::mat4 a = transform(-6, -0.75, 0.6, 0, 0, 0, length, baseHeight, width);
-    cube.setMaterialisticProperty(glm::vec3(0.545, 0.271, 0.075));
-    cube.drawCubeWithMaterialisticProperty(lightingShader2, model * a);
+    cube.drawCubeWithTexture(lightingShader, model * a);
 
     //foam
+    cube.setTextureProperty(bedsheet, bedsheet, 32.0f);
     a = transform(-6, -0.45, 0.6, 0, 0, 0, length, baseHeight - 0.3, width);
-    cube.setMaterialisticProperty(glm::vec3(0.902, 0.384, 0.949));
-    cube.drawCubeWithMaterialisticProperty(lightingShader2, model * a);
+    cube.drawCubeWithTexture(lightingShader, model * a);
 
     //pillow 1
+    cube.setTextureProperty(pillow, pillow, 32.0f);
     a = transform(-5.9, -0.3, 0.7, 0, 0, 0, pillowLength, 0.1, pillowWidth);
-    cube.setMaterialisticProperty(glm::vec3(0.278, 0.635, 0.902));
-    cube.drawCubeWithMaterialisticProperty(lightingShader2, model * a);
+    cube.drawCubeWithTexture(lightingShader, model * a);
 
     ////pillow 2
     a = transform(-5.9, -0.3, 1.3, 0, 0, 0, pillowLength, 0.1, pillowWidth);
-    cube.setMaterialisticProperty(glm::vec3(0.278, 0.635, 0.902));
-    cube.drawCubeWithMaterialisticProperty(lightingShader2, model * a);
+    cube.drawCubeWithTexture(lightingShader, model * a);
 
     ////blanket
     //model = alTogether * translate2 * scale * translate;
     //drawCube(cubeVAO, lightingShader, model, 0.541, 0.169, 0.886);
 
     ////head
+    cube.setTextureProperty(bed_frame, bed_frame, 32.0f);
     a = transform(-6.05, -0.75, 0.6, 0, 0, 0, 0.1, baseHeight + 0.9, width);
-    cube.setMaterialisticProperty(glm::vec3(0.545, 0.271, 0.075));
-    cube.drawCubeWithMaterialisticProperty(lightingShader2, model * a);
+    cube.drawCubeWithTexture(lightingShader, model * a);
 
 }
 
@@ -626,6 +635,159 @@ void doortonextroom(Cube& cube, Shader& lightingShader, Shader& lightingShader2,
     cube.drawCubeWithMaterialisticProperty(lightingShader2, model * a);
 
 }
+
+glm::mat4 transform2(float tx, float ty, float tz, float rx, float ry, float rz, float sx, float sy, float sz)
+{
+    glm::mat4 identityMatrix = glm::mat4(1.0f);
+    glm::mat4 translateMatrix, rotateXMatrix, rotateYMatrix, rotateZMatrix, scaleMatrix, a;
+    translateMatrix = glm::translate(identityMatrix, glm::vec3(tx, ty, tz));
+    rotateXMatrix = glm::rotate(identityMatrix, glm::radians(rx), glm::vec3(1.0f, 0.0f, 0.0f));
+    rotateYMatrix = glm::rotate(identityMatrix, glm::radians(ry), glm::vec3(0.0f, 1.0f, 0.0f));
+    rotateZMatrix = glm::rotate(identityMatrix, glm::radians(rz), glm::vec3(0.0f, 0.0f, 1.0f));
+    scaleMatrix = glm::scale(identityMatrix, glm::vec3(sx, sy, sz));
+    a = translateMatrix  * rotateXMatrix * rotateYMatrix* rotateZMatrix * scaleMatrix;
+    return a;
+}
+
+
+void waredrove(Cube& cube, Shader& lightingShader, Shader& lightingShader2, glm::mat4 model,unsigned int b)
+{
+
+    cube.setTextureProperty(b, b, 32.0f);
+    glm::mat4 a = transform2(-0.3, -0.75, -2.5, 0, 0, 0, 2.0, 3, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.3, -0.75, -2, 0, 90, 0, 1.0, 3, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(0.7, -0.75, -2, 0, 90, 0, 1.0, 3, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+    
+    a = transform2(-0.3, 0.75, -2.5, 90, 0, 0, 2, 1, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.3, -0.73, -2.5, 90, 0, 0, 2, 1, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.3, -0.73+0.375, -2.5, 90, 0, 0, 2, 1, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.3, -0.73+0.75, -2.5, 90, 0, 0, 2, 1, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.3, -0.73 + 1.125, -2.5, 90, 0, 0, 2, 1, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.25, -0.73 + 0.33, -2.499 + shamne1, 90, 0, 0, 1.9, 1, 0.7);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.25, -0.73 + 0.33 + 0.375, -2.499 + shamne2, 90, 0, 0, 1.9, 1, 0.7);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.25, -0.73 + 0.33 + 0.75, -2.499 + shamne3, 90, 0, 0, 1.9, 1, 0.7);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.25, -0.73 + 0.33 + 1.125, -2.499 + shamne4, 90, 0, 0, 1.9, 1, 0.7);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+}
+
+void bedsidetable(Cube& cube, Shader& lightingShader, Shader& lightingShader2, glm::mat4 model)
+{
+
+    cube.setTextureProperty(bedframe, bedframe, 32.0f);
+    glm::mat4 a = transform2(-0.3, -0.75, -2.5, 0, 0, 0, 2.0, 3, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.3, -0.75, -2, 0, 90, 0, 1.0, 3, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(0.7, -0.75, -2, 0, 90, 0, 1.0, 3, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.3, 0.75, -2.5, 90, 0, 0, 2, 1, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.3, -0.73, -2.5, 90, 0, 0, 2, 1, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.3, -0.73 + 0.375, -2.5, 90, 0, 0, 2, 1, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.3, -0.73 + 0.75, -2.5, 90, 0, 0, 2, 1, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.3, -0.73 + 1.125, -2.5, 90, 0, 0, 2, 1, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.25, -0.73 + 0.33, -2.499 + shamne1, 90, 0, 0, 1.9, 1, 0.7);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.25, -0.73 + 0.33 + 0.375, -2.499 + shamne2, 90, 0, 0, 1.9, 1, 0.7);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.25, -0.73 + 0.33 + 0.75, -2.499 + shamne3, 90, 0, 0, 1.9, 1, 0.7);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.25, -0.73 + 0.33 + 1.125, -2.499 + shamne4, 90, 0, 0, 1.9, 1, 0.7);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+
+}
+
+void table2 (Cube& cube, Shader& lightingShader, Shader& lightingShader2, glm::mat4 model)
+{
+
+    cube.setTextureProperty(shelf_tex, shelf_tex, 32.0f);
+    glm::mat4 a = transform2(-0.3, -0.75, -2.5, 0, 0, 0, 2.0, 3, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.3, -0.75, -2, 0, 90, 0, 1.0, 3, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(0.7, -0.75, -2, 0, 90, 0, 1.0, 3, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.3, 0.75, -2.5, 90, 0, 0, 2, 1, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.3, -0.73, -2.5, 90, 0, 0, 2.1, 1, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.3, -0.73 + 0.375, -2.5, 90, 0, 0, 2, 1, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.3, -0.73 + 0.75, -2.5, 90, 0, 0, 2, 1, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.3, -0.73 + 1.125, -2.5, 90, 0, 0, 2, 1, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(0.7, -0.73 + .65, -2.5, 90, 0, 0, 5, 1, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(0.7, -0.75+.45, -2.5, 0, 0, 0, 5.0, 1.8, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.7+3.85, -0.8, -2, 0, 90, 0, 1, 1.4, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(0.7, -0.73 + .45, -2.5, 90, 0, 0, 5, 1, .1);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.25, -0.73 + 0.33, -2.499 + shamne1, 90, 0, 0, 1.9, 1, 0.7);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.25, -0.73 + 0.33 + 0.375, -2.499 + shamne2, 90, 0, 0, 1.9, 1, 0.7);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.25, -0.73 + 0.33 + 0.75, -2.499 + shamne3, 90, 0, 0, 1.9, 1, 0.7);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+
+    a = transform2(-0.25, -0.73 + 0.33 + 1.125, -2.499 + shamne4, 90, 0, 0, 1.9, 1, 0.7);
+    cube.drawCubeWithTexture(lightingShader, model * a);
+}
+
 
 
 
@@ -700,6 +862,25 @@ int main()
     painting_tex = loadTexture("painting.jpg", GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR);
     painting_tex2 = loadTexture("painting2.jpg", GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR);
     painting_tex3 = loadTexture("painting3.jpg", GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR);
+    bed_frame = loadTexture("bed_frame_wood.png", GL_REPEAT, GL_REPEAT, 6, 2);
+    bedframe = loadTexture("bedframe.jpeg", GL_REPEAT, GL_REPEAT, 6, 2);
+    bedsheet = loadTexture("bedsheet.jpeg", GL_REPEAT, GL_REPEAT, 6, 2);
+    pillow = loadTexture("cushion.jpg", GL_REPEAT, GL_REPEAT, 6, 2);
+    table_tex = loadTexture("table wood.png", GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR);
+    brick_wood = loadTexture("brick_wood.png", GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR);
+    
+    string s;
+    for (int i = 0; i < 75; i++)
+    {
+        s = "aquarium/giphy-"+to_string(i)+".png";
+        aquarium = loadTexture(s.c_str(), GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR);
+        if (i == 74)
+            i = 0;
+    }
+
+
+
+
     Cube cube = Cube(diffMap, specMap, 32.0f, 0.0f, 0.0f, 1.0f, 1.0f);
     Cube tiles_cube = Cube(floor_tex, floor_tex, 32.0, 0.0f, 0.0f, 20.0f, 20.0f);
     //Sphere sphere = Sphere();
@@ -782,7 +963,7 @@ int main()
 
 
 
-        model = transform(0, 0, 0, 0, 0, 0, 1, 1, 1);
+        model = transform(-10.2, 0, 2.8, 0, 180, 0, 1.2, 1, 1.6);
         bed(cube, lightingShaderWithTexture, lightingShader, model);
 
 
@@ -823,10 +1004,12 @@ int main()
         table(cube, lightingShaderWithTexture, lightingShader, model);
 
         //tv
-
+        //cout << s << endl;
+        cube.setTextureProperty(aquarium, aquarium, 32.0f);
         a = transform(-2.45, -.4, 1.0, 0, 90, 0, 4.5, 2.2, 0.1);
-        cube.setMaterialisticProperty(glm::vec3(0, 0, 0));
-        cube.drawCubeWithMaterialisticProperty(lightingShader, a);
+        cube.drawCubeWithTexture(lightingShaderWithTexture, a);
+        //cube.setMaterialisticProperty(glm::vec3(0, 0, 0));
+        //cube.drawCubeWithMaterialisticProperty(lightingShader, a);
 
         //painting
         cube.setTextureProperty(painting_tex3, painting_tex3, 32.0);
@@ -848,6 +1031,94 @@ int main()
         model = transform(0, 0, -1, 0, 0, 0, 1, 1, 1);
         shelf(cube, lightingShaderWithTexture, lightingShader, model);
 
+
+        //waredrove
+        model = transform(-5.1, 0, -2, 0, -90, 0, 1, 1, 1);
+        waredrove(cube, lightingShaderWithTexture, lightingShader, model,brick_wood);
+        if(box)
+        {
+            for (int i = 0;i<10;i++)
+            {
+                shamne1 += 0.00013;
+
+                shamne1 = min(shamne1+0.0 , 0.4);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                shamne1 -= 0.00013;
+
+                shamne1 = max(shamne1 + 0.0, 0.0);
+            }
+        }
+
+
+
+        if (box2)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                shamne2 += 0.00013;
+
+                shamne2 = min(shamne2 + 0.0, 0.4);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                shamne2 -= 0.00013;
+
+                shamne2 = max(shamne2 + 0.0, 0.0);
+            }
+        }
+
+        if (box3)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                shamne3 += 0.00013;
+
+                shamne3 = min(shamne3 + 0.0, 0.4);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                shamne3 -= 0.00013;
+
+                shamne3 = max(shamne3 + 0.0, 0.0);
+            }
+        }
+
+        if (box4)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                shamne4 += 0.00013;
+
+                shamne4 = min(shamne4 + 0.0, 0.4);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                shamne4 -= 0.00013;
+
+                shamne4 = max(shamne4 + 0.0, 0.0);
+            }
+        }
+
+            //box
+
+
+        //bed_side_table
+        model = transform(-4.15, -.45, -0.6, 0, -90, 0, 0.35, 0.4, 0.5);
+        bedsidetable(cube, lightingShaderWithTexture, lightingShader, model);
 
         //door to next room
         if (!doortonext)
@@ -916,8 +1187,12 @@ int main()
 
 
         //table
+
+        model = transform2(-4.3 , -0.2, -1, 0, 90, 0, 0.4, 0.7, 1.2);
+        table2(cube, lightingShaderWithTexture, lightingShader, model);
+
         model = transform(-3.15, 0, -3.5, 0, 90, 0, 1, 1, 1);
-        dressingtable(cube, lightingShaderWithTexture, lightingShader, model);
+       // dressingtable(cube, lightingShaderWithTexture, lightingShader, model);
 
 
 
@@ -1042,16 +1317,7 @@ void processInput(GLFWwindow* window)
         }
     }
 
-    if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS)
-    {
-        //angle += 1;
-        if (!dooropen) {
-            dooropen = true;
-        }
-        else {
-            dooropen = false;
-        }
-    }
+
 
 }
 
@@ -1202,7 +1468,55 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
         }
     }
 
+    if (glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS)
+    {
+        if (!box) {
 
+            box = true;
+            //            shamne = min(shamne + 0.0013, 0.4);
+        }
+        else {
+            box = false;
+        }
+    }
+
+
+    if (glfwGetKey(window, GLFW_KEY_N) == GLFW_PRESS)
+    {
+        if (!box2) {
+
+            box2 = true;
+            //            shamne = min(shamne + 0.0013, 0.4);
+        }
+        else {
+            box2 = false;
+        }
+    }
+
+
+    if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS)
+    {
+        if (!box3) {
+
+            box3 = true;
+            //            shamne = min(shamne + 0.0013, 0.4);
+        }
+        else {
+            box3 = false;
+        }
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS)
+    {
+        if (!box4) {
+
+            box4 = true;
+            //            shamne = min(shamne + 0.0013, 0.4);
+        }
+        else {
+            box4 = false;
+        }
+    }
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
